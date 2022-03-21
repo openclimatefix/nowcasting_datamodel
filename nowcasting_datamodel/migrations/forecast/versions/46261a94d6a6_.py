6576f42317f10cd33185705f9076c9bd9aa0def2
peterdudfield <peter.dudfield@hotmail.com>
2022-03-21 09:44:03 +0000

    fix auto alembic

--- nowcasting_datamodel/migrations/forecast/versions/46261a94d6a6_.py
+++ nowcasting_datamodel/migrations/forecast/versions/46261a94d6a6_.py
@@ -45,9 +45,9 @@ def upgrade():  # noqa 103
     op.create_index("ix_datetime_utc", "gsp_yield", [sa.text("datetime_utc DESC")], unique=False)
     op.create_index(op.f("ix_gsp_yield_datetime_utc"), "gsp_yield", ["datetime_utc"], unique=False)
     op.create_index(op.f("ix_gsp_yield_gsp_pk"), "gsp_yield", ["gsp_pk"], unique=False)
+    op.drop_constraint('forecast_location_id_fkey', "forecast", type_="foreignkey")
     op.drop_table("location")
-    op.drop_constraint(None, "forecast", type_="foreignkey")
-    op.create_foreign_key(None, "forecast", "gsp", ["location_id"], ["pk"])
+    op.create_foreign_key("forecast_gsp_id_fkey", "forecast", "gsp", ["location_id"], ["pk"])
     # ### end Alembic commands ###
 
 
