7f3998839219acab1bdca601945ff7f02a8a88ac
peterdudfield <peter.dudfield@hotmail.com>
2022-03-24 07:40:47 +0000

    get national location from database

--- tests/test_national.py
+++ tests/test_national.py
@@ -6,19 +6,19 @@ from nowcasting_datamodel.models import Forecast, ForecastSQL
 from nowcasting_datamodel.national import make_national_forecast
 
 
-def test_make_national_forecast(forecasts_all):
+def test_make_national_forecast(forecasts_all, db_session):
 
     forecasts_all = [Forecast.from_orm(f) for f in forecasts_all]
 
-    national_forecast = make_national_forecast(forecasts=forecasts_all)
+    national_forecast = make_national_forecast(forecasts=forecasts_all,session=db_session)
 
     assert type(national_forecast) == ForecastSQL
 
 
-def test_make_national_forecast_error(forecasts_all):
+def test_make_national_forecast_error(forecasts_all,db_session):
 
     forecasts_all = [Forecast.from_orm(f) for f in forecasts_all]
     forecasts_all[0].location.gsp_id = 2
 
     with pytest.raises(Exception):
-        _ = make_national_forecast(forecasts=forecasts_all)
+        _ = make_national_forecast(forecasts=forecasts_all,session=db_session)
