c8a593a88f5387044cc6656c6d79cb8adee30aea
peterdudfield <peter.dudfield@hotmail.com>
2022-03-31 10:21:23 +0100

    add test if there is no data

--- tests/test_read.py
+++ tests/test_read.py
@@ -196,3 +196,15 @@ def test_update_latest_input_data_last_updated_freeze(db_session):
     input_data_last_updated = get_latest_input_data_last_updated(session=db_session)
     assert input_data_last_updated.pv.replace(tzinfo=None) == now.replace(tzinfo=None)
     assert input_data_last_updated.gsp.replace(tzinfo=None) == yesterday.replace(tzinfo=None)
+
+@freeze_time("2022-01-01")
+def test_update_latest_input_data_last_updated_freeze_no_data(db_session):
+
+    yesterday = datetime.now(tz=timezone.utc) - timedelta(hours=24)
+    now = datetime.now(tz=timezone.utc)
+
+    update_latest_input_data_last_updated(session=db_session, component="pv")
+
+    input_data_last_updated = get_latest_input_data_last_updated(session=db_session)
+    assert input_data_last_updated.pv.replace(tzinfo=None) == now.replace(tzinfo=None)
+    assert input_data_last_updated.gsp.replace(tzinfo=None) == datetime(1960,1,1)
