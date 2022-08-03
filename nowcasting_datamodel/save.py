85768dd07d5bccdfa99735ebff1ecd77e98c81b1
peterdudfield <peter.dudfield@hotmail.com>
2022-08-03 15:05:37 +0200

    add option to save

--- nowcasting_datamodel/save.py
+++ nowcasting_datamodel/save.py
@@ -1,6 +1,6 @@
 """ Save forecasts to the database """
 import logging
-from typing import List
+from typing import List, Optional
 
 from sqlalchemy.orm.session import Session
 
@@ -10,7 +10,12 @@ from nowcasting_datamodel.update import update_all_forecast_latest
 logger = logging.getLogger(__name__)
 
 
-def save(forecasts: List[ForecastSQL], session: Session):
+def save(
+    forecasts: List[ForecastSQL],
+    session: Session,
+    update_national: Optional[bool] = True,
+    update_gsp: Optional[bool] = True,
+):
     """
     Save forecast to database
 
@@ -19,6 +24,8 @@ def save(forecasts: List[ForecastSQL], session: Session):
 
     :param forecasts: list of sql forecasts
     :param session: database session
+    :param update_national: Optional (default true), to update the national forecast
+    :param update_gsp: Optional (default true), to update all the GSP forecasts
     """
 
     # save objects to database
@@ -27,7 +34,9 @@ def save(forecasts: List[ForecastSQL], session: Session):
     session.commit()
 
     logger.debug("Updating to latest")
-    update_all_forecast_latest(session=session, forecasts=forecasts)
+    update_all_forecast_latest(
+        session=session, forecasts=forecasts, update_national=update_national, update_gsp=update_gsp
+    )
     session.commit()
 
 
