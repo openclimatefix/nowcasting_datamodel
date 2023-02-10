0f413391137c4953d437b9c1ff5b3d20bc541993
braddf <bradbdf@gmail.com>
2023-02-10 17:18:53 +0000

    amend query to reach "normal" API speed

--- nowcasting_datamodel/models/convert.py
+++ nowcasting_datamodel/models/convert.py
@@ -2,7 +2,7 @@
 
 from typing import List
 
-from nowcasting_datamodel.models import Forecast, ForecastValue, ForecastValueSevenDaysSQL
+from nowcasting_datamodel.models import Forecast, ForecastSQL, ForecastValue, ForecastValueSevenDaysSQL
 
 
 def convert_list_forecast_value_seven_days_sql_to_list_forecast(
@@ -26,7 +26,15 @@ def convert_list_forecast_value_seven_days_sql_to_list_forecast(
         if gsp_id in forecasts_by_gsp.keys():
             forecasts_by_gsp[gsp_id].forecast_values.append(forecast_value)
         else:
-            forecast = Forecast.from_orm(forecast_value_sql.forecast)
+            forecast = ForecastSQL(
+                model=forecast_value_sql.forecast.model,
+                forecast_creation_time=forecast_value_sql.forecast.forecast_creation_time,
+                location=forecast_value_sql.forecast.location,
+                input_data_last_updated=forecast_value_sql.forecast.input_data_last_updated,
+                forecast_values=[forecast_value_sql],
+                historic=forecast_value_sql.forecast.historic
+            )
+            forecast = Forecast.from_orm(forecast)
             forecasts_by_gsp[gsp_id] = forecast
 
     forecasts = [forecast for forecast in forecasts_by_gsp.values()]
