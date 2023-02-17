2ceeca122b8e2a2964258ed69c33a7bf7e99c67c
peterdudfield <peter.dudfield@hotmail.com>
2023-02-17 14:42:12 +0000

    add function to convert df to forecastsql for national

--- tests/models/test_convert.py
+++ tests/models/test_convert.py
@@ -1,7 +1,11 @@
+import pandas as pd
+from datetime import datetime
+
 from nowcasting_datamodel.fake import make_fake_forecasts
 from nowcasting_datamodel.models import ForecastValueSevenDaysSQL
 from nowcasting_datamodel.models.convert import (
     convert_list_forecast_value_seven_days_sql_to_list_forecast,
+    convert_df_to_national_forecast,
 )
 from nowcasting_datamodel.save import save_all_forecast_values_seven_days
 
@@ -16,10 +20,24 @@ def test_convert_list_forecast_value_seven_days_sql_to_list_forecast(db_session)
     save_all_forecast_values_seven_days(session=db_session, forecasts=forecasts)
     forecast_values = db_session.query(ForecastValueSevenDaysSQL).all()
 
-    print("yyyyy")
-    print(forecast_values[0].forecast.location.gsp_id)
-    print("xxxxxx")
-
     forecast_new = convert_list_forecast_value_seven_days_sql_to_list_forecast(forecast_values)
 
     assert len(forecast_new) == 10
+
+
+def test_convert_df_to_national_forecast(db_session):
+
+    # set up
+    forecast_values_df = pd.DataFrame(
+        columns=["target_datetime_utc", "forecast_mw"],
+        data=[[datetime(2023, 1, 1), 0.0], [datetime(2023, 1, 1, 1), 1.0]],
+    )
+
+    forecast = convert_df_to_national_forecast(
+        session=db_session,
+        forecast_values_df=forecast_values_df,
+        model_name="test_model",
+        version="0.0.1",
+    )
+
+    assert len(forecast.forecast_values) == 2
