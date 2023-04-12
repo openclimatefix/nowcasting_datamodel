aa3b7524311048ae51836ccd9558e148f0c6849e
peterdudfield <peter.dudfield@hotmail.com>
2023-04-12 12:38:12 +0100

    TDD: make failing test

--- tests/read/test_blend.py
+++ tests/read/test_blend.py
@@ -167,6 +167,7 @@ def test_get_blend_forecast_values_latest_two_model_read_two(db_session):
     model_1 = get_model(session=db_session, name="test_1", version="0.0.1")
     model_2 = get_model(session=db_session, name="test_2", version="0.0.1")
 
+    forecasts = {}
     for model in [model_1, model_2]:
         f1 = make_fake_forecasts(gsp_ids=[1, 2], session=db_session)
         f1[0].historic = True
@@ -174,11 +175,12 @@ def test_get_blend_forecast_values_latest_two_model_read_two(db_session):
         if model == model_1:
             power = 1
             adjust = 0
+            forecast_horizon_minutes = [-60, -30, 0, 30, 8 * 30, 15 * 30]
         else:
             power = 2
             adjust = 100
+            forecast_horizon_minutes = [0, 30, 8 * 30, 15 * 30, 16*30]
 
-        forecast_horizon_minutes = [0, 30, 8 * 30, 15 * 30]
         f1[0].forecast_values_latest = [
             ForecastValueLatestSQL(
                 gsp_id=1,
@@ -191,25 +193,31 @@ def test_get_blend_forecast_values_latest_two_model_read_two(db_session):
         ]
 
         db_session.add_all(f1)
-    assert len(db_session.query(ForecastValueLatestSQL).all()) == 8
+        forecasts[model.name] = f1
+    assert len(db_session.query(ForecastValueLatestSQL).all()) == 11
 
     forecast_values_read = get_blend_forecast_values_latest(
         session=db_session,
         gsp_id=f1[0].location.gsp_id,
-        start_datetime=datetime(2023, 1, 1, 0, 0, tzinfo=timezone.utc),
+        start_datetime=datetime(2022, 12, 31, 0, 0, tzinfo=timezone.utc),
         model_names=["test_1", "test_2"],
     )
 
-    assert len(forecast_values_read) == 4
-    assert forecast_values_read[0].target_time == f1[0].forecast_values_latest[0].target_time
+    assert len(forecast_values_read) == 7
+    assert forecast_values_read[0].target_time == (forecasts['test_1'])[0].forecast_values_latest[0].target_time
     assert forecast_values_read[0].expected_power_generation_megawatts == 1
     assert forecast_values_read[1].expected_power_generation_megawatts == 1
-    assert forecast_values_read[2].expected_power_generation_megawatts == 1.5
-    assert forecast_values_read[3].expected_power_generation_megawatts == 2
+    assert forecast_values_read[2].expected_power_generation_megawatts == 1
+    assert forecast_values_read[3].expected_power_generation_megawatts == 1
+    assert forecast_values_read[4].expected_power_generation_megawatts == 1.5
+    assert forecast_values_read[5].expected_power_generation_megawatts == 2
 
     assert forecast_values_read[0]._adjust_mw == 0
-    assert forecast_values_read[2]._adjust_mw == 50
-    assert forecast_values_read[3]._adjust_mw == 100
+    assert forecast_values_read[1]._adjust_mw == 0
+    assert forecast_values_read[2]._adjust_mw == 0
+    assert forecast_values_read[3]._adjust_mw == 0
+    assert forecast_values_read[4]._adjust_mw == 50
+    assert forecast_values_read[5]._adjust_mw == 100
 
 
 @freeze_time("2023-01-01 00:00:01")
