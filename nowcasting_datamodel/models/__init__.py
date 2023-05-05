1ea8fcfc8de65fb0681931a361b0bc1d3aa01929
peterdudfield <peter.dudfield@hotmail.com>
2023-05-05 14:08:41 +0100

    fix

--- nowcasting_datamodel/models/__init__.py
+++ nowcasting_datamodel/models/__init__.py
@@ -20,9 +20,9 @@ Later on it would be good to add a forecast latest table,
 The primary keys could be 'gsp_id' and 'target_datetime_utc'.
 """
 
+from .api import *  # noqa F403
 from .forecast import *  # noqa F403
 from .gsp import *  # noqa F403
 from .metric import *  # noqa F403
 from .models import *  # noqa F403
 from .pv import *  # noqa F403
-from .user import *  # noqa F403
