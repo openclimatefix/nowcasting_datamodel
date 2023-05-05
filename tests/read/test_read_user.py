11e90494244b2881e54773dfb8728e82deec0995
peterdudfield <peter.dudfield@hotmail.com>
2023-05-05 15:03:33 +0100

    fix

--- tests/read/test_read_user.py
+++ tests/read/test_read_user.py
@@ -14,6 +14,6 @@ def test_get_new_metric(db_session):
     assert len(db_session.query(UserSQL).all()) == 0
 
     _ = get_user(session=db_session, name="test@test.com")
-    user = get_user(session=db_session, name="test@test.com")
+    user = get_user(session=db_session, email="test@test.com")
     assert user.email == "test@test.com"
     assert len(db_session.query(UserSQL).all()) == 1
