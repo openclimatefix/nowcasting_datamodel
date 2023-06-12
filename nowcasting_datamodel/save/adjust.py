044fed55dc84238a26a9b218ea817c3c4dad6320
peterdudfield <peter.dudfield@hotmail.com>
2023-06-12 08:22:47 +0100

    force adjust_mw to be 0.0 if me not found

--- nowcasting_datamodel/save/adjust.py
+++ nowcasting_datamodel/save/adjust.py
@@ -80,7 +80,9 @@ def add_adjust_to_national_forecast(forecast: ForecastSQL, session):
             forecast_value.adjust_mw = value
 
         except Exception:
-            logger.debug(f"Could not find ME value for {target_time} in {latest_me_df}")
+            logger.debug(f"Could not find ME value for {target_time} in {latest_me_df}, "
+                         f"therefore adding adjust_mw as 0")
+            forecast_value.adjust_mw = 0.0
 
 
 def get_forecast_horizon_from_forecast(forecast: Union[ForecastSQL, Forecast]):
