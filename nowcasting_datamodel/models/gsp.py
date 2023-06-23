11f261227cdb728c00478ea5af611ee4b2ec56d6
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2023-06-23 14:12:08 +0100

    Issue/add gsp yield columns (#200)
    * add columns to gsp
    
    * add timezone
    
    * add migrations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * lint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * make fields optional
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * only check timezone if not None
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nowcasting_datamodel/models/gsp.py
+++ nowcasting_datamodel/models/gsp.py
@@ -80,6 +80,8 @@ class GSPYieldSQL(Base_Forecast, CreatedMixin):
     datetime_utc = Column(DateTime, index=True)
     solar_generation_kw = Column(Float)
     regime = Column(String, nullable=True)
+    capacity_mwp = Column(Float, nullable=True)
+    pvlive_updated_utc = Column(DateTime, nullable=True)
 
     # many (gsp_yields) to one (location)
     location = relationship("LocationSQL", back_populates="gsp_yields")
@@ -96,8 +98,13 @@ class GSPYield(EnhancedBaseModel):
     regime: str = Field(
         "in-day", description="When the GSP data is pulled, can be 'in-day' or 'day-after'"
     )
+    capacity_mwp: float = Field(None, description="The estimate current capacity")
+    pvlive_updated_utc: datetime = Field(None, description="When PVlive made this value")
 
     _normalize_target_time = validator("datetime_utc", allow_reuse=True)(
+        datetime_must_have_timezone
+    )
+    _normalize_pvlive_updated_utc = validator("pvlive_updated_utc", allow_reuse=True)(
         datetime_must_have_timezone
     )
 
@@ -129,6 +136,8 @@ class GSPYield(EnhancedBaseModel):
             datetime_utc=self.datetime_utc,
             solar_generation_kw=self.solar_generation_kw,
             regime=self.regime,
+            capacity_mwp=self.capacity_mwp,
+            pvlive_updated_utc=self.pvlive_updated_utc,
         )
 
 
