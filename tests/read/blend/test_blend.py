f9496a5b39ac30aa8b4ed9e0a053820365558ed1
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2023-07-17 07:48:54 +0100

    TDD: add test (#219)
    * TDD: add test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * the fix, reset indexes so adjust works
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/read/blend/test_blend.py
+++ tests/read/blend/test_blend.py
@@ -180,7 +180,80 @@ def test_get_blend_forecast_values_latest_two_model_read_two(db_session):
     assert forecast_values_read[5]._adjust_mw == 200
 
 
-#
+@freeze_time("2023-01-01 00:00:01")
+def test_get_blend_forecast_values_two_models_plevel_second(db_session):
+    """This test checks that the blend is done correctly when the plevel is the second"""
+    model_1 = get_model(session=db_session, name="test_1", version="0.0.1")
+    model_2 = get_model(session=db_session, name="test_2", version="0.0.1")
+
+    forecasts = {}
+    for model in [model_1, model_2]:
+        f1 = make_fake_forecasts(gsp_ids=[1, 2], session=db_session)
+        f1[0].historic = True
+
+        if model == model_1:
+            power = 1
+            adjust = 0
+            forecast_horizon_minutes = [-60, -30, 0, 30, 8 * 30, 15 * 30]
+            created_utc = datetime(2023, 1, 1, 0, 0, 1, tzinfo=timezone.utc)
+            properties = {"10": 0.9, "90": 1.1}
+        elif model == model_2:
+            power = 2
+            adjust = 100
+            forecast_horizon_minutes = [0, 30, 8 * 30, 15 * 30, 16 * 30]
+            created_utc = datetime(2023, 1, 1, 0, 0, 1, tzinfo=timezone.utc)
+            properties = {"10": 1.8, "90": 2.2}
+
+        f1[0].forecast_values_latest = [
+            ForecastValueLatestSQL(
+                gsp_id=1,
+                expected_power_generation_megawatts=power,
+                target_time=datetime(2023, 1, 1, tzinfo=timezone.utc) + timedelta(minutes=t),
+                model_id=model.id,
+                adjust_mw=adjust,
+                created_utc=created_utc,
+                properties=properties,
+            )
+            for t in forecast_horizon_minutes
+        ]
+
+        db_session.add_all(f1)
+        forecasts[model.name] = f1
+    assert len(db_session.query(ForecastValueLatestSQL).all()) == 11
+
+    forecast_values_read = get_blend_forecast_values_latest(
+        session=db_session,
+        gsp_id=f1[0].location.gsp_id,
+        start_datetime=datetime(2022, 12, 31, 0, 0, tzinfo=timezone.utc),
+        model_names=["test_2", "test_1"],
+        properties_model="test_1",
+    )
+
+    assert len(forecast_values_read) == 7
+    assert (
+        forecast_values_read[0].target_time
+        == (forecasts["test_1"])[0].forecast_values_latest[0].target_time
+    )
+    assert forecast_values_read[0].expected_power_generation_megawatts == 1.0  # test_1
+    assert forecast_values_read[1].expected_power_generation_megawatts == 1.0  # test_1
+    assert forecast_values_read[2].expected_power_generation_megawatts == 2  # test_2
+    assert forecast_values_read[3].expected_power_generation_megawatts == 2  # test_2
+    assert forecast_values_read[4].expected_power_generation_megawatts == 1.5  # mix
+    assert forecast_values_read[5].expected_power_generation_megawatts == 1  # test_1
+
+    assert forecast_values_read[0]._properties == {"10": 0.9, "90": 1.1}
+    assert forecast_values_read[1]._properties == {"10": 0.9, "90": 1.1}
+    assert forecast_values_read[2]._properties == {"10": 1.9, "90": 2.1}
+    assert forecast_values_read[3]._properties == {"10": 1.9, "90": 2.1}
+    assert forecast_values_read[4]._properties == {"10": 1.4, "90": 1.6}
+    assert forecast_values_read[5]._properties == {"10": 0.9, "90": 1.1}
+
+    assert forecast_values_read[0]._adjust_mw == 0
+    assert forecast_values_read[1]._adjust_mw == 0
+    assert forecast_values_read[2]._adjust_mw == 100
+    assert forecast_values_read[3]._adjust_mw == 100
+    assert forecast_values_read[4]._adjust_mw == 50
+    assert forecast_values_read[5]._adjust_mw == 0
 
 
 @freeze_time("2023-01-01 00:00:01")
