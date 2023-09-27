c141aafcb00653c49f00660de7a3f03585e41056
peterdudfield <peter.dudfield@hotmail.com>
2023-09-27 16:45:33 +0100

    undo

--- nowcasting_datamodel/models/forecast.py
+++ nowcasting_datamodel/models/forecast.py
@@ -333,7 +333,6 @@ class ForecastValue(EnhancedBaseModel):
             target_time=self.target_time,
             expected_power_generation_megawatts=self.expected_power_generation_megawatts,
             adjust_mw=self._adjust_mw,
-            properties=self._properties,
         )
 
     @classmethod
