c35663f5f5325c73c6b862b8f25f31ce4a466641
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2023-12-04 16:44:08 +0000

    add read api calls + tests (#253)
    * add read api calls + tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update ruff
    
    * update doc string
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nowcasting_datamodel/read/read_user.py
+++ nowcasting_datamodel/read/read_user.py
@@ -1,9 +1,10 @@
 """ Read user"""
 import logging
+from typing import List
 
 from sqlalchemy.orm.session import Session
 
-from nowcasting_datamodel.models.api import UserSQL
+from nowcasting_datamodel.models.api import APIRequestSQL, UserSQL
 
 logger = logging.getLogger(__name__)
 
@@ -40,3 +41,42 @@ def get_user(session: Session, email: str) -> UserSQL:
         user = users[0]
 
     return user
+
+
+def get_all_last_api_request(session: Session) -> List[APIRequestSQL]:
+    """
+    Get all last api requests for all users
+
+    :param session: database session
+    :return:
+    """
+
+    last_requests_sql = (
+        session.query(APIRequestSQL)
+        .distinct(APIRequestSQL.user_uuid)
+        .join(UserSQL)
+        .order_by(APIRequestSQL.user_uuid, APIRequestSQL.created_utc.desc())
+        .all()
+    )
+
+    return last_requests_sql
+
+
+def get_api_requests_for_one_user(session: Session, email: str) -> List[APIRequestSQL]:
+    """
+    Get all api requests for one user
+
+    :param session: database session
+    :param email: user email
+    :return:
+    """
+
+    api_requests = (
+        session.query(APIRequestSQL)
+        .join(UserSQL)
+        .filter(UserSQL.email == email)
+        .order_by(APIRequestSQL.created_utc.desc())
+        .all()
+    )
+
+    return api_requests
