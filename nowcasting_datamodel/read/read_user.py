129c2000eb30d23c4c05b0d90ec1b5bfc087be41
Sahil Chhoker <chhokersahil435@gmail.com>
2024-11-28 19:29:14 +0530

    Add URL Filtering Capabilities to API Request Retrieval Functions (#299)
    * feat: Implement URL filtering for API request retrieval
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added tests for include_url and exclude_url
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nowcasting_datamodel/read/read_user.py
+++ nowcasting_datamodel/read/read_user.py
@@ -15,56 +15,53 @@ logger = logging.getLogger(__name__)
 def get_user(session: Session, email: str) -> UserSQL:
     """
     Get metric object from database
-
     :param session: database session
     :param email: email of user
-
     return: One Metric SQl object
-
     """
-
     # start main query
     query = session.query(UserSQL)
-
     # filter on name
     query = query.filter(UserSQL.email == email)
-
     # get all results
     users = query.all()
-
     if len(users) == 0:
         logger.debug(f"User for name {email} does not exist so going to add it")
-
         user = UserSQL(email=email)
-
         session.add(user)
         session.commit()
-
     else:
         user = users[0]
-
     return user
 
 
-def get_all_last_api_request(session: Session) -> List[APIRequestSQL]:
+def get_all_last_api_request(
+    session: Session, include_in_url: Optional[str] = None, exclude_in_url: Optional[str] = None
+) -> List[APIRequestSQL]:
     """
     Get all last api requests for all users
-
     :param session: database session
-    :return:
+    :param include_in_url: Optional filter to include only URLs containing this string
+    :param exclude_in_url: Optional filter to exclude URLs containing this string
+    :return: List of last API requests
     """
-
-    last_requests_sql = (
+    query = (
         session.query(APIRequestSQL)
         .distinct(APIRequestSQL.user_uuid)
         .join(UserSQL)
         .options(contains_eager(APIRequestSQL.user))
         .populate_existing()
         .order_by(APIRequestSQL.user_uuid, APIRequestSQL.created_utc.desc())
-        .all()
     )
 
-    return last_requests_sql
+    # Apply URL filtering if specified
+    if include_in_url is not None:
+        query = query.filter(APIRequestSQL.url.like(f"%{include_in_url}%"))
+
+    if exclude_in_url is not None:
+        query = query.filter(~APIRequestSQL.url.like(f"%{exclude_in_url}%"))
+
+    return query.all()
 
 
 def get_api_requests_for_one_user(
@@ -72,16 +69,18 @@ def get_api_requests_for_one_user(
     email: str,
     start_datetime: Optional[datetime] = None,
     end_datetime: Optional[datetime] = None,
+    include_in_url: Optional[str] = None,
+    exclude_in_url: Optional[str] = None,
 ) -> List[APIRequestSQL]:
     """
     Get all api requests for one user
-
     :param session: database session
     :param email: user email
     :param start_datetime: only get api requests after start datetime
     :param end_datetime: only get api requests before end datetime
+    :param include_in_url: Optional filter to include only URLs containing this string
+    :param exclude_in_url: Optional filter to exclude URLs containing this string
     """
-
     query = session.query(APIRequestSQL).join(UserSQL).filter(UserSQL.email == email)
 
     if start_datetime is not None:
@@ -90,6 +89,11 @@ def get_api_requests_for_one_user(
     if end_datetime is not None:
         query = query.filter(APIRequestSQL.created_utc <= end_datetime)
 
-    api_requests = query.order_by(APIRequestSQL.created_utc.desc()).all()
+    # Apply URL filtering if specified
+    if include_in_url is not None:
+        query = query.filter(APIRequestSQL.url.like(f"%{include_in_url}%"))
 
-    return api_requests
+    if exclude_in_url is not None:
+        query = query.filter(~APIRequestSQL.url.like(f"%{exclude_in_url}%"))
+
+    return query.order_by(APIRequestSQL.created_utc.desc()).all()
