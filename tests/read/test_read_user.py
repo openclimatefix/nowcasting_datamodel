7cc5cb02c44a4871ca08ba73346e48d28b1c5d83
sahil-chhoker <chhokersahil435@gmail.com>
2024-11-28 18:24:51 +0530

    added tests for include_url and exclude_url

--- tests/read/test_read_user.py
+++ tests/read/test_read_user.py
@@ -39,10 +39,26 @@ def test_get_all_last_api_request(db_session):
 def test_get_api_requests_for_one_user(db_session):
     user = get_user(session=db_session, email="test@test.com")
     db_session.add(APIRequestSQL(user_uuid=user.uuid, url="test"))
-
+    db_session.add(APIRequestSQL(user_uuid=user.uuid, url="test2"))
+    
     requests_sql = get_api_requests_for_one_user(session=db_session, email=user.email)
-    assert len(requests_sql) == 1
-    assert requests_sql[0].url == "test"
+    assert len(requests_sql) == 2 
+
+    include_filtered = get_api_requests_for_one_user(
+        session=db_session,
+        email=user.email,
+        include_in_url="test"
+    )
+    assert len(include_filtered) == 1
+    assert include_filtered[0].url == "test"
+
+    exclude_filtered = get_api_requests_for_one_user(
+        session=db_session,
+        email=user.email,
+        exclude_in_url="test2"
+    )
+    assert len(exclude_filtered) == 1
+    assert exclude_filtered[0].url == "test"
 
 
 def test_get_api_requests_for_one_user_start_datetime(db_session):
