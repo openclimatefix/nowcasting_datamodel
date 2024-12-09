410aa512fb6ab433bda355451949c1aba3103b41
devsjc <47188100+devsjc@users.noreply.github.com>
2024-12-09 16:43:36 +0000

    2030 partitions (#304)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nowcasting_datamodel/models/forecast.py
+++ nowcasting_datamodel/models/forecast.py
@@ -153,37 +153,6 @@ class ForecastValueSQL(
     forecast = relationship("ForecastSQL", back_populates="forecast_values")
 
 
-def get_partitions(start_year: int, start_month: int, end_year: int, end_month: int):
-    """Make partitions"""
-    partitions = []
-    for year in range(start_year, end_year + 1):
-        if year != start_year:
-            start_month = 1
-        if year == end_year:
-            end_month_loop = end_month
-        else:
-            end_month_loop = 13
-
-        for month in range(start_month, end_month_loop):
-            if month == 12:
-                year_end = year + 1
-                month_end = 1
-            else:
-                year_end = year
-                month_end = month + 1
-
-            if month < 10:
-                month = f"0{month}"
-            if month_end < 10:
-                month_end = f"0{month_end}"
-
-            partitions.append(
-                ForecastValueSQL.create_partition(f"{year}_{month}", f"{year_end}_{month_end}")
-            )
-
-    return partitions
-
-
 def create_forecastvalueyearmonth_class(year, month):
     """Dynamically create a ForecastValueYearMonthClass dynamically for input year and month"""
 
@@ -254,7 +223,7 @@ def make_partitions(start_year: int, start_month: int, end_year: int):
             )
 
 
-make_partitions(2022, 8, 2025)
+make_partitions(2022, 8, 2030)
 
 
 # legacy table, this means migration still work
